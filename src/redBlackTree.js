6d4c05a5ea79f3960a9956bd5fa1f8fa3482969c
jan-wong <543050768@qq.com>
2020-02-03 20:52:07 +0800

    红黑树插入

--- src/redBlackTree.js
+++ src/redBlackTree.js
@@ -9,6 +9,10 @@
  * 定理1：一棵有n个内部节点的红黑树的高度至多为2log(n+1)
  */
 
+// 常量
+const RED = 'red'
+const BLACK = 'black'
+
 /**
  * 左旋转
  */
@@ -51,5 +55,85 @@ function rightRotate(T, y) {
   // 将y的父节点变为x
   y.p = x
   x.right = y
+}
+
+/**
+ * 插入
+ */
+function RBInsert(T, z) {
+  let y = T.nil // y表示z插入的父节点
+  let x = T.root
+  // 获取z的父节点
+  while(x !== T.nil) {
+    y = x
+    if (z.key < x.key)
+      y = x.left
+    else
+      y = x.right
+  }
+  // 设置z的父节点为y
+  z.p = y
+  // 设置父节点y的孩纸节点
+  if (y === T.nil)
+    T.root = z
+  else if (z.key < y.key)
+    y.left = z
+  else
+    y.right = z
+  // 设置z的孩纸节点及颜色
+  z.left = T.nil
+  z.right = T.nil
+  z.color = RED
+  // 保持红黑树的性质
+  RBInsertFixup(T, z)
+}
+
+/**
+ * 保持红黑树的性质
+ */
+function RBInsertFixup(T, z) {
+  // z和z.p都为红色节点
+  while(z.p.color === RED) {
+    // 如果z的父节点是祖父节点的左孩纸
+    if (z.p === z.p.p.left) {
+      // 获取z的祖父节点的右孩纸
+      let y = z.p.p.right
+      // 1.如果叔父节点为红色(可以确定的是：z为红色，z的父节点为红色，z的祖父节点为黑色，z的叔父节点为红色)
+      // 开始变色
+      if (y.color === RED) {
+        z.p.color = BLACK
+        z.p.p.color = RED
+        y.color = BLACK
+        z = z.p.p
+      // 2.如果z是父节点的右孩纸(可以确定的是：z为红色，z的父节点为红色，z的祖父节点为黑色，z的叔父节点为黑色)
+      // 开始左旋转
+      } else if (z === z.p.right) {
+        z = z.p
+        leftRotate(T, z)
+      // 3.如果z是父节点的左孩纸(可以确定的是：z为红色，z的父节点为红色，z的祖父节点为黑色，z的叔父节点为黑色)
+      // 开始变色和右旋转
+      } else {
+        z.p.color = BLACK
+        z.p.p.color = RED
+        rightRotate(T, z.p.p)
+      }
+    } else {
+      let y = z.p.p.left
+      if (y.color === RED) {
+        z.p.color = BLACK
+        z.p.p.color = RED
+        y.color = BLACK
+        z = z.p.p
+      } else if (z === z.p.right) {
+        z = z.p
+        leftRotate(T, z)
+      } else {
+        z.p.color = BLACK
+        z.p.p.color = RED
+        rightRotate(T, z.p.p)
+      }
+    }
+  }
+  T.root.color = BLACK
 }
 
